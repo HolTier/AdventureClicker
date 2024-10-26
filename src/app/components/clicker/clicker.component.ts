814a0ed21dda4d6d7aeb31f5c41b21cb8c418bb5
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-10-26 14:00:39 +0200

    Add "+1" text during click event on div.

--- src/app/components/clicker/clicker.component.ts
+++ src/app/components/clicker/clicker.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgForOf, NgOptimizedImage, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-clicker',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgForOf,
+    NgStyle
   ],
   templateUrl: './clicker.component.html',
   styleUrl: './clicker.component.css'
 })
 export class ClickerComponent {
+  // Take img from local assets
   imgSrc: string = "/images/orcAI1.webp"
+  clicks: { x: number; y: number; value: string }[] = [];
 
-  ClickerComponent(){
-    // Take img from local assets
-    //this.imgSrc = "/src/assets/images/orcAI1.webp";
+  onClickerClick(event: MouseEvent): void {
+    // Take coordinates
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // Add +1 to list
+    this.clicks.push({x, y, value: '+1'})
+
+    // Delete +1 from list
+    setTimeout(() => {
+      this.clicks.shift();
+    }, 1000)
   }
 }
