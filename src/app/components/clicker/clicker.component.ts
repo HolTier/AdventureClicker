4f5276d5c5c183080119900ef20a54b030556221
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-10-26 13:06:43 +0200

    Add an image in clicker component

--- src/app/components/clicker/clicker.component.ts
+++ src/app/components/clicker/clicker.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-clicker',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './clicker.component.html',
   styleUrl: './clicker.component.css'
 })
 export class ClickerComponent {
+  imgSrc: string = "/images/orcAI1.webp"
 
+  ClickerComponent(){
+    // Take img from local assets
+    //this.imgSrc = "/src/assets/images/orcAI1.webp";
+  }
 }
