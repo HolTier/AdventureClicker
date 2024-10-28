681ec8d1d1147b0ebddb8d3e8307e3952399146b
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-10-28 09:29:35 +0100

    Add coins counter.

--- src/app/components/clicker/clicker.component.ts
+++ src/app/components/clicker/clicker.component.ts
@@ -22,17 +22,24 @@ export class ClickerComponent {
   enemyHealth: number = 100;
   healthValue: any = 100;
   images: ImageService = new ImageService();
+  coins: number = 0;
 
   onClickerClick(event: MouseEvent): void {
-    // Take coordinates
-    const x = event.clientX;
-    const y = event.clientY;
+    const target = event.currentTarget as HTMLElement; // Get the clicked element
+    const rect = target.getBoundingClientRect(); // Get the element's position and dimensions
+
+    // Adjust coordinates
+    const x = event.clientX - rect.left + 6;
+    const y = event.clientY - rect.top - 12;
 
     // Add +1 to list
     this.clicks.push({x, y, value: '+1'})
 
     // Decrease the hp from monster
     this.healthValue -= 1;
+
+    // Increase coins
+    this.coins += 1;
 
     if (this.healthValue <= 0) {
       this.healthValue = 100;
