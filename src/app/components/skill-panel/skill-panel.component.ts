897d3f63c9855aa65987ff3ab0ce2cd792d339ec
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-11-21 02:17:19 +0100

    Generated skill panel component.

---
+++ src/app/components/skill-panel/skill-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill-panel.component.html',
+  styleUrl: './skill-panel.component.css'
+})
+export class SkillPanelComponent {
+
+}
